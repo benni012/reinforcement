0c027df86d802fa7399a01608d5235938908db15
Ben Chwalek <ben.chwalek@rwth-aachen.de>
2023-08-26 15:50:05 +0200

    add score and end condition
    Squashed commit of the following:
    
    commit 533414199bbc2a432f0ec8746b86e061bd387941
    Author: Ben Chwalek <ben.chwalek@rwth-aachen.de>
    Date:   Sat Aug 26 15:49:58 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:reinforcement.py
    
    commit 5dbee8078b240ed74b0dd42581b7a40ffe1fe6da
    Author: fsteinfeld <fabian.steinfeld@rwth-aachen.de>
    Date:   Fri Aug 25 16:45:01 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:reinforcement.py
    
    commit 54e46781e235c3b82f7801843826719084176591
    Author: Ben Chwalek <ben.chwalek@rwth-aachen.de>
    Date:   Fri Aug 25 16:28:15 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:reinforcement.py
    
    commit fbc623b9895496fda45667a964de653f3cffb652
    Author: fsteinfeld <fabian.steinfeld@rwth-aachen.de>
    Date:   Fri Aug 25 16:01:25 2023 +0200
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:reinforcement.py
    
    commit dfbc58031113ab93b3a5368a6cfa2501f228f706
    Author: fsteinfeld <fabian.steinfeld@rwth-aachen.de>
    Date:   Fri Aug 25 15:54:23 2023 +0200
    
        mob start [ci-skip] [ci skip] [skip ci]
    
    Co-authored-by: fsteinfeld <fabian.steinfeld@rwth-aachen.de>

--- reinforcement.py
+++ reinforcement.py
@@ -5,7 +5,7 @@ import random
 
 
 SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 800
+SCREEN_HEIGHT = 700
 CIRCLE_RADIUS = 40
 
 def clamp(a, min, max):
@@ -43,32 +43,32 @@ class Circle:
     velocity: Vector
     radius: float
     color: int
+    dead: bool
 
 colors = [arcade.color.WHITE_SMOKE, arcade.color.BABY_BLUE, arcade.color.RACKLEY, arcade.color.ALABAMA_CRIMSON]
 
-class MyGame(arcade.Window):
-    def __init__(self, width, height):
-        super().__init__(width, height, "Simple Arcade Game")
+class MyGame():
+    def __init__(self):
         self.left = self.right = self.up = self.down = False
-        self.player = Circle(Vector(SCREEN_WIDTH//2,SCREEN_HEIGHT//2), Vector(0,0), 20, 0)
+        self.player = Circle(Vector(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2), Vector(0, 0), 20, 0, False)
         self.entities = [self.player]
+        self.score = self.player.radius * self.player.radius
+        self.end = False
 
         for i in range(100):
             self.entities.append(Circle(Vector(random.randrange(SCREEN_WIDTH), random.randrange(SCREEN_HEIGHT)),
                                         Vector(random.randrange(100), random.randrange(100)),
-                                        random.randrange(10,20),
-                                        random.randrange(1,len(colors))))
-
-    def on_draw(self):
-        arcade.start_render()
-        for e in self.entities:
-            if e is None: continue
-            arcade.draw_circle_filled(e.position.x, e.position.y, e.radius, colors[e.color])
-    
+                                        random.randrange(10, 20),
+                                        random.randrange(1, len(colors)),
+                                        False))
 
     def on_update(self, delta_time):
         speed = 200
-        vel = Vector(0,0)
+
+        self.score = self.player.radius * self.player.radius if not self.player.dead else 0
+        if self.end: return
+
+        vel = Vector(0, 0)
         if self.left:
             vel.x = -1
         if self.right:
@@ -77,61 +77,79 @@ class MyGame(arcade.Window):
             vel.y = 1
         if self.down:
             vel.y = -1
-        
+
         vel = vel.normalized()
         self.player.velocity = vel * speed
 
         for e in self.entities:
-            if e is None: continue
+            if e.dead: continue
             e.position += e.velocity * delta_time
 
-            if e.position.x < e.radius or e.position.x >= SCREEN_WIDTH-e.radius:
+            if e.position.x < e.radius or e.position.x >= SCREEN_WIDTH - e.radius:
                 e.velocity.x = -e.velocity.x
-                e.position.x = clamp(e.position.x, e.radius, SCREEN_WIDTH-e.radius-1)
-            if e.position.y < e.radius or e.position.y >= SCREEN_HEIGHT-e.radius:
+                e.position.x = clamp(e.position.x, e.radius, SCREEN_WIDTH - e.radius - 1)
+            if e.position.y < e.radius or e.position.y >= SCREEN_HEIGHT - e.radius:
                 e.velocity.y = -e.velocity.y
-                e.position.y = clamp(e.position.y, e.radius, SCREEN_HEIGHT-e.radius-1)
-        
+                e.position.y = clamp(e.position.y, e.radius, SCREEN_HEIGHT - e.radius - 1)
+
         for i in range(len(self.entities)):
-            for j in range(i+1, len(self.entities)):
+            for j in range(i + 1, len(self.entities)):
                 a, b = self.entities[i], self.entities[j]
-                if a is None or b is None: continue
+                if a.dead or b.dead: continue
                 d = a.position.dist(b.position)
-                if d < a.radius+b.radius:
+                if d < a.radius + b.radius:
                     if a.radius > b.radius:
-                        a.radius = (d + math.sqrt(2*(a.radius*a.radius+b.radius*b.radius)-d*d))/2
+                        a.radius = (d + math.sqrt(2 * (a.radius * a.radius + b.radius * b.radius) - d * d)) / 2
                         b.radius = d - a.radius
                     else:
-                        b.radius = (d + math.sqrt(2*(a.radius*a.radius+b.radius*b.radius)-d*d))/2
+                        b.radius = (d + math.sqrt(2 * (a.radius * a.radius + b.radius * b.radius) - d * d)) / 2
                         a.radius = d - b.radius
 
                     if a.radius < 1:
-                        self.entities[i] = None
+                        self.entities[i].dead = True
                     if b.radius < 1:
-                        self.entities[j] = None
+                        self.entities[j].dead = True
+
+        self.end = all(entity.dead for entity in self.entities[1:]) or self.player.dead
+
+class GameWrapper(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height, "Simple Arcade Game")
+        self.game = MyGame()
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text(self.game.score, 10, 10)
+        for e in self.game.entities:
+            if e.dead: continue
+            arcade.draw_circle_filled(e.position.x, e.position.y, e.radius, colors[e.color])
+    
+
+    def on_update(self, delta_time):
+        self.game.on_update(delta_time)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
-            self.left = True
+            self.game.left = True
         elif key == arcade.key.RIGHT:
-            self.right = True
+            self.game.right = True
         elif key == arcade.key.UP:
-            self.up = True
+            self.game.up = True
         elif key == arcade.key.DOWN:
-            self.down = True
+            self.game.down = True
 
     def on_key_release(self, key: int, modifiers: int):
         if key == arcade.key.LEFT:
-            self.left = False
+            self.game.left = False
         elif key == arcade.key.RIGHT:
-            self.right = False
+            self.game.right = False
         elif key == arcade.key.UP:
-            self.up = False
+            self.game.up = False
         elif key == arcade.key.DOWN:
-            self.down = False 
+            self.game.down = False
 
 def main():
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = GameWrapper(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
 
 if __name__ == "__main__":
